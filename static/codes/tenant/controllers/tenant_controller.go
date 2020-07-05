88957ae303dfacad1af1a2799429330b661c45c6
zoetrope <a.ikezoe@gmail.com>
2020-07-05 18:28:44 +0900

    add rbac markers

--- static/codes/tenant/controllers/tenant_controller.go
+++ static/codes/tenant/controllers/tenant_controller.go
@@ -20,6 +20,8 @@ type TenantReconciler struct {
 
 // +kubebuilder:rbac:groups=multitenancy.example.com,resources=tenants,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=multitenancy.example.com,resources=tenants/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=core,resources=namespaces,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=rbac,resources=rolebindings,verbs=get;list;watch;create;update;patch;delete
 
 func (r *TenantReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 	_ = context.Background()
