edfa7c459f4d246d5cd6b413d922a6c6862b15e5
zoetrope <a.ikezoe@gmail.com>
2020-07-15 23:21:00 +0900

    add webhook test

---
+++ static/codes/tenant/api/v1/tenant_webhook_test.go
@@ -0,0 +1,45 @@
+package v1
+
+import (
+	"context"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	rbacv1 "k8s.io/api/rbac/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+var _ = Describe("Tenant Webhook", func() {
+	It("should create a valid tenant", func() {
+		ctx := context.Background()
+		tenant := &Tenant{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: GroupVersion.String(),
+				Kind:       "Tenant",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "test",
+			},
+			Spec: TenantSpec{
+				Namespaces: []string{
+					"test1",
+					"test2",
+				},
+				NamespacePrefix: "",
+				Admin: rbacv1.Subject{
+					Kind:      "ServiceAccount",
+					Name:      "default",
+					Namespace: "default",
+				},
+			},
+		}
+		err := k8sClient.Create(ctx, tenant)
+		Expect(err).Should(Succeed())
+
+		mutatedTenant := &Tenant{}
+		err = k8sClient.Get(ctx, types.NamespacedName{Name: "test"}, mutatedTenant)
+		Expect(err).Should(Succeed())
+		Expect(mutatedTenant.Spec.NamespacePrefix).Should(Equal("test-"))
+	})
+})
